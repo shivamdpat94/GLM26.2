163e0a30bebab595a40ea5417f42b44ff7b44f30
shiva <shivamdpat94@gmail.com>
2022-09-13 21:43:07 -0400

    Created virtual environment for minikube

--- run_api.sh
+++ run_api.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 
+
+
+
+
+#Requires Docker and Minikube
+
+
 echo 1-Container             2-Cluster?
 read option
 
 if [ $option -eq 1 ]
 then
 ################  ONLY NEEDED TO RUN ONCE. CAN BE REMOVED ONCE IMAGE IS ON DEVICE ##############
-        docker pull shivamdpat94/dockerhub:myfirstimagepush
+        docker pull shivamdpat94/dockerhub2:myfirstimagepush
 ################################################################################################
-        docker run -d -p 5000:5000 shivamdpat94/dockerhub:myfirstimagepush
+        docker run -d -p 5000:5000 shivamdpat94/dockerhub2:myfirstimagepush
 fi
 
 
@@ -19,14 +26,20 @@ fi
 
 if [ $option -eq 2 ]
 then
-################  ONLY NEEDED TO RUN ONCE. CAN BE REMOVED ONCE IMAGE IS ON DEVICE ##############
-        docker pull shivamdpat94/dockerhub:myfirstimagepush
-################################################################################################
+
         minikube start
 
-################  ONLY NEEDED TO RUN ONCE. CAN BE REMOVED ONCE IMAGE IS ON DEVICE
-        minikube image load shivamdpat94/dockerhub:myfirstimagepush
+        eval $(minikube docker-env)
+
+         git clone https://github.com/shivamdpat94/GLM26.2.git
+
+         cd GLM26.2/
+
+
+################  ONLY NEEDED TO RUN ONCE. CAN BE REMOVED ONCE IMAGE IS ON DEVICE ##############
+        docker pull shivamdpat94/dockerhub2:myfirstimagepush
 ################################################################################################
+
 	      kubectl apply -f deployment.yaml
 	      minikube service flask-test-service
 
